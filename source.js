0496c254d97a0dec2ce1847f51c0a91d7c069926
karianjahi <kitgaedesigaidi8@gmail.com>
2025-10-31 07:46:44 +0100

    Initialize the function that sums all numbers between including show numbers

--- source.js
+++ source.js
@@ -1,2 +1,11 @@
+/*
+Take an array of two numbers and returns the sum of those two numbers plust the sum of all the numbers between them
+*/
 
+const sumAll = (anArrayOfTwoNumbers) => {
+    return anArrayOfTwoNumbers;
+}
 
+// Test
+let myArray = [10, 7];
+console.log(sumAll(myArray));

d5ed4aeabb4fa9004387894a91bde42065e5eb08
karianjahi <kitgaedesigaidi8@gmail.com>
2025-10-31 08:15:01 +0100

    Complete and test the function

--- source.js
+++ source.js
@@ -4,10 +4,11 @@ Take an array of two numbers and returns the sum of those two numbers plust the 
 
 const sumAll = (anArrayOfTwoNumbers) => {
     if (anArrayOfTwoNumbers[0] > anArrayOfTwoNumbers[1]) anArrayOfTwoNumbers = [anArrayOfTwoNumbers[1], anArrayOfTwoNumbers[0]];
-    return anArrayOfTwoNumbers;
-
+    let sumNos = 0
+    for (let i=anArrayOfTwoNumbers[0]; i < anArrayOfTwoNumbers[1]+1; i++) sumNos += i
+    return sumNos;
 }
 
 // Test
-let myArray = [10, 7];
+let myArray = [10, 5];
 console.log(sumAll(myArray));
